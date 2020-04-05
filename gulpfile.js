3e3af6cfd39a82ca948f14278909fdad9c61e51d
Maxkindev <maxkindev@gmail.com>
2020-04-05 18:54:28 +0300

    Add next plugins: gulp-sass, gulp-sourcemaps, gulp-babel, gulp-uglify, gulp-concat. Add watchers for js and scss files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,35 @@
+'use strict';
 const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
 const browserSync = require('browser-sync').create();
+const babel = require('gulp-babel'); // npm install --save-dev gulp-babel @babel/core @babel/preset-env
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+
+function bundleJs (cb) {
+  gulp.src('./src/assets/js/scripts/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(uglify())
+    .pipe(concat('bundle.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./src/assets/js'))
+    .pipe(browserSync.stream());
+  cb();
+}
+
+function compileStyles (cb) {
+  gulp.src('./src/assets/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./src/assets/css'))
+    .pipe(browserSync.stream());
+  cb();
+}
 
 function initLocalServer(cb) {
   browserSync.init({
@@ -13,11 +43,17 @@ function initLocalServer(cb) {
 }
 
 function watchEverything(cb) {
+  gulp.watch('./src/assets/js/scripts/**/*.scss', bundleJs);
+  gulp.watch('./src/assets/scss/**/*.scss', compileStyles);
   gulp.watch('./src/**/**/*.html').on('change', browserSync.reload);
   cb();
 }
 
-exports.default = gulp.parallel(
-  initLocalServer,
-  watchEverything
+exports.default = gulp.series( 
+  bundleJs,
+  compileStyles, 
+  gulp.parallel(
+    initLocalServer,
+    watchEverything
+  )
 );
